05e9953f92181f1fe62f09132812c5086591c850
rf777rf777 <rf777rf777@gmail.com>
2024-09-12 00:07:25 +0800

    update embed content

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,13 +1,16 @@
 import discord
 from discord.ext import commands
-from modules.view import PaginationView
-from modules.instagram_retriever import InstagramItem
+from modules.pagination_view import PaginationView
+from modules.instagram_item import InstagramItem
 
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.ig_item = InstagramItem('yuzuki_yzk030')
-        #self.ig_item = InstagramItem('duckie_elle')
+        #self.ig_item = InstagramItem('yuzuki_yzk030')
+        #self.ig_item = InstagramItem('momiko_124')
+        #self.ig_item = InstagramItem('eeelyeee')
+        self.ig_item = InstagramItem('walkerpretty96')
+
         #self.profile = ig_item.get_user_profile()
         self.latest_post = None
 
@@ -21,26 +24,33 @@ class Commands(commands.Cog):
         if len(caption) > 256:
             caption = caption[:256]  # 截斷到 256 個字元
         
+        hashtags = self.latest_post['hashtages']
+        
         embed = discord.Embed(
             title=caption,
-            description=self.latest_post['hashtages'],
+            description=hashtags if len(hashtags) > 0 else '',
             color=0x00ff00
         )
-        
-        embed.set_footer(text=self.latest_post['code'])
-        contents = self.latest_post['content']
-        for url, type in contents.items(): 
-            if type == 'image':
-                embed.set_image(url=url)
-            else:
-                embed.set_image(url=url)
-                embed.url = url
+        embed.url = f'https://www.instagram.com/p/{self.latest_post['code']}'
+        #embed.set_footer(text=f'https://www.instagram.com/p/{self.latest_post['code']}')
+        post_items = self.latest_post['post_items']
+        item = post_items[index]
+        if item.type == 'image':
+            embed.set_image(url=item.url)
+        else:
+            embed.set_image(url=item.url)
+            embed.add_field(
+                name="影片連結：",
+                value=f"[點擊這裡觀看]({item.video_url})",
+                inline=False
+            )            
+
         return embed
 
     @commands.command()
     async def show_items(self, ctx):
         self.latest_post = self.ig_item.get_latest_posts()
-        view = PaginationView(total_items=len(self.latest_post['content']), update_embed_callback=self.generate_embed)
+        view = PaginationView(total_items=len(self.latest_post['post_items']), update_embed_callback=self.generate_embed)
         embed = self.generate_embed(0)
         await ctx.send(embed=embed, view=view)
 
--- modules/instagram_item.py
+++ modules/instagram_item.py
@@ -1,5 +1,11 @@
 import instaloader
 
+class PostItem():
+    def __init__(self, type, url, video_url=None):
+        self.type = type
+        self.url = url
+        self.video_url = video_url
+
 class InstagramItem():
     def __init__(self, username):
         ig_loader = instaloader.Instaloader()
@@ -15,6 +21,7 @@ class InstagramItem():
     def get_latest_posts(self, ignore_pinned=True):
         posts_iterator = self.profile.get_posts()
           
+        #最後latest_posts_count個貼文
         latest_posts = []
         latest_posts_count = 4 if ignore_pinned else 1
         for _ in range(latest_posts_count):
@@ -23,27 +30,31 @@ class InstagramItem():
         dates = [post.date for post in latest_posts]
         latest_date = max(dates)
     
-        # 找到對應的貼文
+        # 最後一個貼文
         latest_post = latest_posts[dates.index(latest_date)] 
-               
-        content_urls = {}
+         
+        # 製作PostItem      
+        post_items = []
         if latest_post.typename == 'GraphSidecar':
             for node in latest_post.get_sidecar_nodes():
                 if node.is_video:
-                    content_urls[node.video_url] = 'video'
+                    post_items.append(
+                        PostItem('video', node.url, node.video_url))
                 else:
-                    content_urls[node.display_url] = 'image'
+                    post_items.append(
+                        PostItem('image', node.display_url))
         else:
             # 單張圖片或單部影片
             if latest_post.is_video:
-                content_urls[latest_post.url] = 'video'
-                #content_urls[latest_post.video_url] = 'video'
+                post_items.append(
+                    PostItem('video', latest_post.url, latest_post.video_url))
             else:
-                content_urls[latest_post.url] = 'image'                
+                post_items.append(
+                    PostItem('image', latest_post.url))
         return {
             'caption': latest_post.caption,
             'hashtages': latest_post.caption_hashtags,
             'code': latest_post.shortcode,
-            'content': content_urls         
+            'post_items': post_items         
         }
 
